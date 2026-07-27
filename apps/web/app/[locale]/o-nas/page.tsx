71ec9bffeaaad3c0ab424314cc124306d57a6c76
Tetiana Proshyna <t.proshyna@cyfrowe.org>
2026-07-27 23:23:19 +0200

    test ISR

--- apps/web/app/[locale]/o-nas/page.tsx
+++ apps/web/app/[locale]/o-nas/page.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { q } from "@/sanity/groqd";
 import { sanityFetchProduction } from "@/sanity/live";
-import { SanitySections } from "@/sanity/sections/SanitySections";
+// import { SanitySections } from "@/sanity/sections/SanitySections";
 import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 // import ContactForm from "@/components/reusable/contactForm/ContactForm";
-// import HeaderTest from "@/components/HeaderTest";
+import HeaderTest from "@/components/HeaderTest";
 
 export const metadata: Metadata = {
   title: "O nas | Stowarzyszenie Akwedukt",
@@ -34,11 +34,11 @@ export default async function ONas({ params }: { params: Promise<{ locale: strin
         na Figmie brak - tymczasowo wyłączone
         <Breadcrumbs items={[{ label: "Strona główna", href: `/${locale}` }, { label: "O nas" }]} />
        */}
-      {/*<HeaderTest*/}
-      {/*  headerText="Z lokalnych źródeł płyniemy w świat"*/}
-      {/*  subHeading="Odkrywaj z nami nowe możliwości"*/}
-      {/*/>*/}
-      <SanitySections value={page?.sections} />
+      <HeaderTest
+        headerText="Z lokalnych źródeł płyniemy w świat"
+        subHeading="Odkrywaj z nami nowe możliwości"
+      />
+      {/*<SanitySections value={page?.sections} />*/}
       {/*<ContactForm*/}
       {/*  headingText="Napisz do nas"*/}
       {/*  subHeadingText="Masz pytanie, problem lub propozycję? Wyślij wiadomość, skontaktujemy się z Tobą najszybciej jak to możliwe."*/}
