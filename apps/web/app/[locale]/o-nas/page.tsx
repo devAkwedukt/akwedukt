4a2edba8c797416354a65af4034d5cc7fa77575a
Tetiana Proshyna <t.proshyna@cyfrowe.org>
2026-07-27 23:52:05 +0200

    test ISR

--- apps/web/app/[locale]/o-nas/page.tsx
+++ apps/web/app/[locale]/o-nas/page.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { q } from "@/sanity/groqd";
 import { sanityFetchProduction } from "@/sanity/live";
-// import { SanitySections } from "@/sanity/sections/SanitySections";
+import { SanitySections } from "@/sanity/sections/SanitySections";
 import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
-// import ContactForm from "@/components/reusable/contactForm/ContactForm";
+import ContactForm from "@/components/reusable/contactForm/ContactForm";
 import HeaderTest from "@/components/HeaderTest";
 
 export const metadata: Metadata = {
@@ -38,11 +38,11 @@ export default async function ONas({ params }: { params: Promise<{ locale: strin
         headerText="Z lokalnych źródeł płyniemy w świat"
         subHeading="Odkrywaj z nami nowe możliwości"
       />
-      {/*<SanitySections value={page?.sections} />*/}
-      {/*<ContactForm*/}
-      {/*  headingText="Napisz do nas"*/}
-      {/*  subHeadingText="Masz pytanie, problem lub propozycję? Wyślij wiadomość, skontaktujemy się z Tobą najszybciej jak to możliwe."*/}
-      {/*/>*/}
+      <SanitySections value={page?.sections} />
+      <ContactForm
+        headingText="Napisz do nas"
+        subHeadingText="Masz pytanie, problem lub propozycję? Wyślij wiadomość, skontaktujemy się z Tobą najszybciej jak to możliwe."
+      />
     </>
   );
 }
