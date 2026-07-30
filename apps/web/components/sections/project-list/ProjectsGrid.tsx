0e97aead74972598643428aa163a8d5449a8449f
Tetiana Proshyna <t.proshyna@cyfrowe.org>
2026-07-30 16:55:34 +0200

    fix build error

--- apps/web/components/sections/project-list/ProjectsGrid.tsx
+++ apps/web/components/sections/project-list/ProjectsGrid.tsx
@@ -31,9 +31,6 @@ export function ProjectsGrid({ projects, ctaText = "Dowiedz się więcej" }: Pro
 
             {/* TEXT CONTENT OF PROJECT */}
             <div className="pt-6 flex flex-col grow">
-              {project.startDate && (
-                <div className="text-md">{new Date(project.startDate).toLocaleDateString()}</div>
-              )}
               <Link href={`/project/${project.slug?.current}`} className="heading-3 mb-2 w-fit">
                 {project.title}
               </Link>
